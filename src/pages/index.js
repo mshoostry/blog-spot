7bd9bc7b339a89eef5308c9def0e7bf6c2d07f3a
mshoostry@gmail.com <mshoostry@Mehnazs-MacBook-Pro.local>
2020-12-07 16:54:41 -0600

    changes in index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ const IndexPage = () => (
     <h2 style={{ fontStyle: "italic", fontWeight: 300, textAlign: "center" }}>Words are magical and musical. So, gather around to share, inspire and enjoy the musical notes!!!!!</h2>
     <h2 style={{ fontStyle: "italic", fontWeight: 300, textAlign: "center" }}>Let this be a medium for the young and old to contribute into the world through vibrancy of alphabets, 
         put together into sentences that further lead us to appreciate the Blogs…..</h2>
-    <Link to="/blog">Click here and let's get started!</Link>
+    <Link to="/blog">Click here to read the blogs...let's get started!</Link>
   </Layout>
 )
 
